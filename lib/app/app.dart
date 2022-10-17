55db3eed04e566de3d14a21eefa18a210297c680
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-17 23:44:44 +0545

    add routes generator to routes manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '/presentation/theme_manager.dart';
+import '/presentation/resources/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp._internal(); // private named constructor
--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_mvvm/presentation/forgot_password/forgot_password.dart';
+import 'package:flutter_clean_mvvm/presentation/login/login.dart';
+import 'package:flutter_clean_mvvm/presentation/main/main_view.dart';
+import 'package:flutter_clean_mvvm/presentation/on_boarding/on_boarding.dart';
+import 'package:flutter_clean_mvvm/presentation/register/register.dart';
+
+import 'package:flutter_clean_mvvm/presentation/splash/splash.dart';
+import 'package:flutter_clean_mvvm/presentation/store_details/store_details.dart';
+
 class Routes {
   static const String splashRoute = '/';
   static const String onBoardingRoute = '/onBoarding';
@@ -6,4 +16,38 @@ class Routes {
   static const String forgetPasswordRoute = '/forgetPassword';
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('No Route Found!'),
+        ),
+        body: const Center(child: Text('No Route Found!')),
+      ),
+    );
+  }
 }
