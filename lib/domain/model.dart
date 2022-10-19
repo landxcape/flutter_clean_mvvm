ee362659527c1c4b651fa4113c25ede72888a6bb
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-19 16:45:48 +0545

    Update model.dart

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -1,7 +1,46 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class SliderObject {
   String title;
   String subTitle;
   String image;
 
-  SliderObject(this.title, this.subTitle, this.image);
+  SliderObject(
+    this.title,
+    this.subTitle,
+    this.image,
+  );
+}
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(
+    this.id,
+    this.name,
+    this.numOfNotifications,
+  );
+}
+
+class Contacts {
+  String email;
+  String phone;
+  String link;
+
+  Contacts(
+    this.email,
+    this.phone,
+    this.link,
+  );
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(
+    this.customer,
+    this.contacts,
+  );
 }
