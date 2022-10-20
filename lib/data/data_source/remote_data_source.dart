986a25d5a3e3509061e5826ec328d127507e851a
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-20 11:28:39 +0545

    add remote data source

---
+++ lib/data/data_source/remote_data_source.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_clean_mvvm/data/network/app_api.dart';
+import 'package:flutter_clean_mvvm/data/request/request.dart';
+import 'package:flutter_clean_mvvm/data/responses/responses.dart';
+
+abstract class RemoteDataSource {
+  Future<AuthenticationResponse> login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImplementer implements RemoteDataSource {
+  final AppServiceClient _appServiceClient;
+  RemoteDataSourceImplementer(this._appServiceClient);
+
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
+    return await _appServiceClient.login(
+      loginRequest.email,
+      loginRequest.password,
+      loginRequest.imei,
+      loginRequest.deviceType,
+    );
+  }
+}
