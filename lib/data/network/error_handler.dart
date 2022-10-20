ff7e80eb8afe06f60c76eb88474848c0e52dd9d4
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-20 12:54:10 +0545

    add response messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -33,3 +33,23 @@ class ResponseCode {
   static const int cacheError = -6;
   static const int noInternetConnection = -7;
 }
+
+class ResponseMessage {
+  // API status codes
+  static const String success = 'success'; // success with data
+  static const String noContent = 'success with no content'; // success with no content
+  static const String badRequest = 'bad request, try again later'; // failure, api rejected the request
+  static const String forbidden = 'forbidden request, try again later'; // failure, api rejected the request
+  static const String unauthorised = 'user is unauthorized, try again later'; // user is not authorized
+  static const String notFound = 'Url is not found, try again later'; // failure, api url incorrect, not found
+  static const String internalServerError = 'something is wrong, try again later'; // failure, crash happened in server side
+
+  // local status codes
+  static const String unknown = 'something is wrong, try again later';
+  static const String connectTimeout = 'time out error, try again later';
+  static const String cancel = 'request was cancelled, try again later';
+  static const String receiveTimeout = 'time out error, try again later';
+  static const String sendTimeout = 'time out error, try again later';
+  static const String cacheError = 'cache error, try again later';
+  static const String noInternetConnection = 'Please check your internet connection';
+}
