2b8cf21acfc3a88fa22842aac25ef44535a8a2e4
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-24 22:49:56 +0545

    add stream builders inside register view

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_mvvm/app/dependency_injection.dart';
+import 'package:flutter_clean_mvvm/presentation/common/state_renderer/state_renderer_impl.dart';
 import 'package:flutter_clean_mvvm/presentation/register/register_viewmodel.dart';
+import 'package:flutter_clean_mvvm/presentation/resources/color_manager.dart';
+import 'package:flutter_clean_mvvm/presentation/resources/values_manager.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -41,6 +44,26 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        elevation: AppSize.s0,
+        iconTheme: IconThemeData(color: ColorManager.primary),
+        backgroundColor: ColorManager.white,
+      ),
+      body: StreamBuilder<FlowState>(
+        stream: _viewModel.outputState,
+        builder: (context, snapshot) {
+          return snapshot.data?.getScreenWidget(context, _getContentWidget(), () {
+                _viewModel.register();
+              }) ??
+              _getContentWidget();
+        },
+      ),
+    );
+  }
+
+  Widget _getContentWidget() {
     return Container();
   }
 }
