96f5e51a266f4762250277389e79660ebedb588d
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-24 14:19:05 +0545

    add register user api and repository

--- lib/data/request/request.dart
+++ lib/data/request/request.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class LoginRequest {
   String email;
   String password;
@@ -9,5 +10,21 @@ class LoginRequest {
     this.password,
     this.imei,
     this.deviceType,
+  );
+}
+
+class RegisterRequest {
+  String countryMobileCode;
+  String username;
+  String email;
+  String password;
+  String profilePicture;
+
+  RegisterRequest(
+    this.countryMobileCode,
+    this.username,
+    this.email,
+    this.password,
+    this.profilePicture,
   );
 }
--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -8,4 +8,5 @@ import '../../data/request/request.dart';
 abstract class Repository {
   Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
   Future<Either<Failure, String>> forgotPassword(String email);
+  Future<Either<Failure, Authentication>> registerUser(Repository repository);
 }
