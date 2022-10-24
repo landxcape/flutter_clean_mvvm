72e1589728792ac3a3dceecaff940a4bc1c19556
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-24 20:09:54 +0545

    implement register viewmodel inputs and outputs

--- lib/presentation/register/register_viewmodel.dart
+++ lib/presentation/register/register_viewmodel.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:analyzer/file_system/file_system.dart';
+import 'package:flutter_clean_mvvm/domain/usecase/register_usecase.dart';
 import 'package:flutter_clean_mvvm/presentation/base/baseviewmodel.dart';
+import 'package:flutter_clean_mvvm/presentation/resources/strings_manager.dart';
 
-class RegisterViewModel extends BaseViewModel {
+class RegisterViewModel extends BaseViewModel with RegisterViewModelInput, RegisterViewModelOutput {
   final StreamController _mobileNumberStreamController = StreamController<String>.broadcast();
   final StreamController _usernameStreamController = StreamController<String>.broadcast();
   final StreamController _emailStreamController = StreamController<String>.broadcast();
@@ -12,6 +14,10 @@ class RegisterViewModel extends BaseViewModel {
 
   final StreamController _isAllInputsValid = StreamController<void>.broadcast();
 
+  final RegisterUseCase _registerUseCase;
+  RegisterViewModel(this._registerUseCase);
+
+  // inputs
   @override
   void start() {
     // TODO: implement start
@@ -26,6 +32,67 @@ class RegisterViewModel extends BaseViewModel {
     _profilePictureStreamController.close();
     _isAllInputsValid.close();
   }
+
+  @override
+  Sink get inputEmail => _emailStreamController.sink;
+
+  @override
+  Sink get inputMobileNumber => _mobileNumberStreamController.sink;
+
+  @override
+  Sink get inputPassword => _passwordStreamController.sink;
+
+  @override
+  Sink get inputProfilePicutre => _profilePictureStreamController.sink;
+
+  @override
+  Sink get inputUsername => _usernameStreamController.sink;
+
+  // outputs
+  @override
+  // TODO: implement outputIsUsernameValid
+  Stream<bool> get outputIsUsernameValid => _usernameStreamController.stream.map((username) => _isUsernameValid(username));
+
+  @override
+  // TODO: implement outputErrorUsername
+  Stream<String?> get outputErrorUsername => outputIsUsernameValid.map((isUsernameValid) => isUsernameValid ? null : AppStrings.usernameError);
+
+  @override
+  // TODO: implement outputIsEmailValid
+  Stream<bool> get outputIsEmailValid => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputErrorEmail
+  Stream<String?> get outputErrorEmail => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputIsMobileNumberValid
+  Stream<bool> get outputIsMobileNumberValid => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputErrorMobileNumber
+  Stream<String?> get outputErrorMobileNumber => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputIsPasswordValid
+  Stream<bool> get outputIsPasswordValid => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputErrorPassword
+  Stream<String?> get outputErrorPassword => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputIsProfilePictureValid
+  Stream<File> get outputIsProfilePictureValid => throw UnimplementedError();
+
+  @override
+  register() {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+
+  // private methods
+  bool _isUsernameValid(String username) => username.length >= 8;
 }
 
 abstract class RegisterViewModelInput {
@@ -44,16 +111,16 @@ abstract class RegisterViewModelInput {
 
 abstract class RegisterViewModelOutput {
   Stream<bool> get outputIsUsernameValid;
-  Stream<String> get outputErrorUsername;
+  Stream<String?> get outputErrorUsername;
 
   Stream<bool> get outputIsMobileNumberValid;
-  Stream<String> get outputErrorMobileNumber;
+  Stream<String?> get outputErrorMobileNumber;
 
   Stream<bool> get outputIsEmailValid;
-  Stream<String> get outputErrorEmail;
+  Stream<String?> get outputErrorEmail;
 
   Stream<bool> get outputIsPasswordValid;
-  Stream<String> get outputErrorPassword;
+  Stream<String?> get outputErrorPassword;
 
   Stream<File> get outputIsProfilePictureValid;
 }
