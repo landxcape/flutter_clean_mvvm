de50b9ca53c91c051d2e32cd9d12b49d18971c12
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-26 13:05:16 +0545

    Create home_usecase.dart

---
+++ lib/domain/usecase/home_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_clean_mvvm/data/network/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_mvvm/domain/repository/repository.dart';
+import 'package:flutter_clean_mvvm/domain/usecase/base_usecase.dart';
+
+import '../model/model.dart';
+
+class HomeUseCase extends BaseUseCase<void, HomeObject> {
+  final Repository _repository;
+  HomeUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, HomeObject>> execute(void input) async {
+    return await _repository.getHome();
+  }
+}
