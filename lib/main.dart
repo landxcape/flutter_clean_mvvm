a475ebe71d220a4ffd8e12cf7d1ed3a40d5c07d6
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-30 21:56:54 +0545

    update main with localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 // Flutter imports:
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_mvvm/presentation/resources/language_manager.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 // Project imports:
 import '/app/dependency_injection.dart';
@@ -7,7 +10,11 @@ import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await EasyLocalization.ensureInitialized();
   await initAppModule();
-  runApp(MyApp());
+  runApp(EasyLocalization(
+    supportedLocales: const [englishLocale, nepaliLocale],
+    path: assetsPathLocalisation,
+    child: Phoenix(child: MyApp()),
+  ));
 }
