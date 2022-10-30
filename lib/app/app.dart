9f0fd97b7e19fa75c6498072b6d7e7b43cf8043a
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2022-10-30 22:05:13 +0545

    update application class and settings with localization

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,8 @@
 // Flutter imports:
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_mvvm/app/app_prefs.dart';
+import 'package:flutter_clean_mvvm/app/dependency_injection.dart';
 
 // Project imports:
 import '/presentation/resources/routes_manager.dart';
@@ -17,9 +20,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocale().then((local) => context.setLocale(local));
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       theme: getApplicationTheme(),
       onGenerateRoute: RouteGenerator.getRoute,
